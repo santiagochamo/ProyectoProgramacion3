2f544f13de8e2a768cc84af98e43cf40ec9960ba
30Thiago <grimthiago@gmail.com>
2023-04-10 12:18:06 -0300

    ultimamos navbar

--- proyectointegrador3/src/Screens/Home/Home.js
+++ proyectointegrador3/src/Screens/Home/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Peliculas from "../../Components/Peliculas/Peliculas"
-import NavBar from "../../Components/NavBar/NavBar"
 
 function Home() {
     return (
         <>
-            <NavBar />
         <h1>
             <Peliculas />
         </h1>
---
+++ proyectointegrador3/src/Screens/VerTodasPeli/VerTodasPeli.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class VerTodasPeli extends Component {
+  render() {
+    return (
+      <div>VerTodasPeli</div>
+    )
+  }
+}
