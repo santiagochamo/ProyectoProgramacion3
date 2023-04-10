0526edfe5dbc5bef381afabf72e3c5d1d689f715
30Thiago <grimthiago@gmail.com>
2023-04-10 09:43:40 -0300

    avances de detalle series

--- proyectointegrador3/src/Screens/DetalleSerie/DetalleSerie.js
+++ proyectointegrador3/src/Screens/DetalleSerie/DetalleSerie.js
@@ -1,29 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class DetalleSerie extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      id:this.props.match.params.id,
-      series: [],
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: props.match.params.id,
+            serie:{},
+            generos: [],
+            
+        }
+
     }
-}
 
-componentDidMount(){
-  fetch()
-  .then(resp => resp.json)
-  .then(data => 
-    this.setState({series:data}))
-  .catch (e => console.log(e))
-}
-render() {
-  return (
-    <div>
-       <p>info de cada serie</p>
-    </div>
-  )
-}
-}
+    componentDidMount() {
 
+       //fetch
+       console.log(this.state.serie)
+       fetch(`https://api.themoviedb.org/3/tv/popular/${this.state.id}?api_key=32a583d4ccec7f702faad954f990f1ba`)
+       .then(resp => resp.json())
+       .then(data => this.setState({
+         serie : data ,
+         generos : data.genres ,
+       }))
+       .catch(e => console.log(e))
+         
+       
+    }
+    render(){
+        return (
+            <React.Fragment>
+                    { 
+                        this.state.serie.title === 0 ? 
+                        <div>
+                            <p>Cargando... </p>
+                        </div> 
+                        : 
+                        <div>
+                            <h2>{this.state.serie.title}</h2>
+                            
+                            <div className=''>
 
-export default  DetalleSerie
+                                <div className=''>
+                                    <img src={`https://image.tmdb.org/t/p/w342/${this.state.serie.poster_path}`} alt={this.state.serie.title} />
+                                </div>
+
+                                <div className=''>
+                                    <h3>Rating: {this.state.serie.popularity}</h3>
+                                    <h3>Fecha de estreno: {this.state.serie.release_date}</h3>
+                                    <p>Sinopsis: {this.state.serie.overview} </p>
+                                    <ul>
+                                       {this.state.generos.map((genero , idx)  =>  
+                                       <li propKey= {genero.name + idx} >
+                                         {genero.name}
+                                       </li>        
+                                       )}
+                                    </ul>
+                                    
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    }
+            </React.Fragment>
+        )
+    }
+
+    }
+
+export default DetalleSerie
